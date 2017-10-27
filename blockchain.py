dddd1d8fdb4aedf416e55cca9a392bbef44639b9
Radhika1997 <rdj.150697@gmail.com>
2017-10-27 11:37:59 +0530

    fix of typo errors

--- blockchain.py
+++ blockchain.py
@@ -127,7 +127,7 @@ class Blockchain:
         return self.last_block['index'] + 1
 
     @property
-    def last_block(self) -> Dict[str: Any]:
+    def last_block(self) -> Dict[str, Any]:
         return self.chain[-1]
 
     @staticmethod
